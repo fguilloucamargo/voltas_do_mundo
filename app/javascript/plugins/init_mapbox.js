87a2ed8feae9c8d5f1ad2f26aed42e652bcf8773
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-09-08 12:20:35 +0200

    changing zoom map for mobile device

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,61 +3,113 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
   var count = 0;
-  // var colors = ['#2F5755', '#54C6BE', '#F7B15C', '#F65C51', '#E5243F'];
   var colors = ['#A364D9', '#ED657B', '#DB3839', '#F66320', '#F9A229', '#FECC30', '#B2C324', '#34BEB7', '#3FA5D8']
 
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/fguilloucamargo/ckd75pwh20e6g1io4utxsda1f',
-      center: [0, 15],
-      zoom: 1.2
-    });
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-      new mapboxgl.Marker( {color: '#3d348b', scale: 0.5})
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(map);
-    });
-
-    fetch("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson")
-    .then(reponse => reponse.json())
-    .then((data) => {
-      data.features.forEach (element => {
-        if(mapElement.dataset.country.includes(element.properties.admin)) {
-          map.on('load', function() {
-          if(count < colors.length - 1) {
-            count = count + 1;
-          } else {
-            count = 0;
-          }
-            map.addSource(element.properties.admin, {
-              "type": "geojson",
-              "data": {
-                "type": "Feature",
-                "geometry": element.geometry
-              }
-            });
-            map.addLayer({
-              id: element.properties.admin,
-              type: 'fill',
-              source: element.properties.admin,
-              paint: {
-                'fill-color': colors[count],
-                'fill-opacity': 0.9
-                }
-            });
-            map.moveLayer(element.properties.admin, 'country-label');
-            map.moveLayer(element.properties.admin, 'state-label');
-            map.moveLayer(element.properties.admin, 'settlement-major-label');
-            map.moveLayer(element.properties.admin, 'settlement-minor-label');
-          })
-        };
+    if (screen.width >= 768) {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/fguilloucamargo/ckd75pwh20e6g1io4utxsda1f?optimize=true',
+        center: [0, 15],
+        zoom: 1.2
       });
-    })
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+        new mapboxgl.Marker( {color: '#3d348b', scale: 0.5})
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+      });
+
+      fetch("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson")
+      .then(reponse => reponse.json())
+      .then((data) => {
+        data.features.forEach (element => {
+          if(mapElement.dataset.country.includes(element.properties.admin)) {
+            map.on('load', function() {
+              if(count < colors.length - 1) {
+                count = count + 1;
+              } else {
+                count = 0;
+              }
+              map.addSource(element.properties.admin, {
+                "type": "geojson",
+                "data": {
+                  "type": "Feature",
+                  "geometry": element.geometry
+                }
+              });
+              map.addLayer({
+                id: element.properties.admin,
+                type: 'fill',
+                source: element.properties.admin,
+                paint: {
+                  'fill-color': colors[count],
+                  'fill-opacity': 0.9
+                  }
+              });
+              map.moveLayer(element.properties.admin, 'country-label');
+              map.moveLayer(element.properties.admin, 'state-label');
+              map.moveLayer(element.properties.admin, 'settlement-major-label');
+              map.moveLayer(element.properties.admin, 'settlement-minor-label');
+            })
+          };
+        });
+      })
+    } else {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/fguilloucamargo/ckd75pwh20e6g1io4utxsda1f?optimize=true',
+        center: [-10, 10],
+        zoom: 0
+      });
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+        new mapboxgl.Marker( {color: '#3d348b', scale: 0.5})
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+      });
+
+      fetch("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson")
+      .then(reponse => reponse.json())
+      .then((data) => {
+        data.features.forEach (element => {
+          if(mapElement.dataset.country.includes(element.properties.admin)) {
+            map.on('load', function() {
+              if(count < colors.length - 1) {
+                count = count + 1;
+              } else {
+                count = 0;
+              }
+              map.addSource(element.properties.admin, {
+                "type": "geojson",
+                "data": {
+                  "type": "Feature",
+                  "geometry": element.geometry
+                }
+              });
+              map.addLayer({
+                id: element.properties.admin,
+                type: 'fill',
+                source: element.properties.admin,
+                paint: {
+                  'fill-color': colors[count],
+                  'fill-opacity': 0.9
+                  }
+              });
+              map.moveLayer(element.properties.admin, 'country-label');
+              map.moveLayer(element.properties.admin, 'state-label');
+              map.moveLayer(element.properties.admin, 'settlement-major-label');
+              map.moveLayer(element.properties.admin, 'settlement-minor-label');
+            })
+          };
+        });
+      })
+    }
   };
 };
 
